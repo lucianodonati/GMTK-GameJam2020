b51db450e558323be69b4eabcc7cc50f6390e5eb
Enzo Rolón <enzo.rolon@outlook.com>
2020-07-11 15:34:58 -0300

    ImageNamer script

---
+++ Assets/Scripts/EditorTools/ImageNamer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageNamer : MonoBehaviour
+{
+    [SerializeField]Image[] objectImages = null;
+
+    [Button(ButtonStyle.Box, Name = "Rename all images")]
+    public void RenameImage()
+    {
+        objectImages = GetComponentsInChildren<Image>();
+
+        foreach (Image image in objectImages)
+        {
+            image.gameObject.name = image.sprite.name;
+        }
+    }
+}
