d53fc9676a3b2eb16336bd1b3e1b91c0afb9af62
Luciano Donati <me@lucianodonati.com>
2020-07-11 20:51:31 -0300

    Created file creator script

---
+++ Assets/Scripts/FileCreator.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class FileCreator
+{
+    private string path;
+    private bool initialized;
+
+    private void Initialize()
+    {
+        path = Application.dataPath;
+        if (Application.platform == RuntimePlatform.OSXPlayer)
+        {
+            path += "/../../";
+        }
+        else if (Application.platform == RuntimePlatform.WindowsPlayer)
+        {
+            path += "/../";
+        }
+
+        initialized = true;
+    }
+
+    public void CreateTxtFile(string name, string content)
+    {
+        if (!initialized)
+            Initialize();
+
+        System.IO.File.WriteAllText($@"{path}\{name}.txt", content);
+    }
+}
