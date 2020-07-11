31682774261ae44f0e1b323793816f01488b3c73
Luciano Donati <me@lucianodonati.com>
2020-07-11 16:34:56 -0300

    Tweaked ImageNamer

--- Assets/Scripts/EditorTools/ImageNamer.cs
+++ Assets/Scripts/EditorTools/ImageNamer.cs
@@ -6,12 +6,10 @@ using UnityEngine.UI;
 
 public class ImageNamer : MonoBehaviour
 {
-    [SerializeField]Image[] objectImages = null;
-
     [Button(ButtonStyle.Box, Name = "Rename all images")]
     public void RenameImage()
     {
-        objectImages = GetComponentsInChildren<Image>();
+        Image[] objectImages = GetComponentsInChildren<Image>();
 
         foreach (Image image in objectImages)
         {
