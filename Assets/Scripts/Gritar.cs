aee14e75fdce752f4ed7616676958fd548f76d2a
Luciano Donati <me@lucianodonati.com>
2020-07-11 16:36:25 -0300

    Added test script

---
+++ Assets/Scripts/Gritar.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gritar : MonoBehaviour
+{
+    public void HolaSi()
+    {
+        Debug.Log("AHAHAHSAJKFDHSKJDFHKSJDFHSKD");
+    }
+}
