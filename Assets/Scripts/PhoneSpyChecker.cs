c61f3b363ef51125de803fdbd976185b82d2d900
Luciano Donati <me@lucianodonati.com>
2020-07-12 07:06:42 -0300

    Added script to check password

---
+++ Assets/Scripts/PhoneSpyChecker.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PhoneSpyChecker : MonoBehaviour
+{
+    [SerializeField]
+    private string correctPasscode;
+
+    [SerializeField]
+    private UnityEvent onCorrectPasscode = null;
+
+    [SerializeField]
+    private UnityEvent onIncorrectPasscode = null;
+
+    public void CheckPasscode(string passcode)
+    {
+        if (correctPasscode.Equals(passcode))
+            onCorrectPasscode.Invoke();
+        else
+            onIncorrectPasscode.Invoke();
+    }
+}
